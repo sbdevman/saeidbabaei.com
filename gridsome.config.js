0de0818f1420b1e47ee75e9f8ad3f7448b8c02ad
Saeid <saeid.babaei@outlook.com>
2021-11-07 21:59:59 +0300

    b1

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteDescription: "Saeid Babaei's Personal Website",
   siteUrl: 'https://saeidbabaei.com',
   titleTemplate: `%s | Saeid Babaei`,
-  icon: 'src/favicon.ico',
+  icon: 'src/favicon.png',
 
   transformers: {
     remark: {
